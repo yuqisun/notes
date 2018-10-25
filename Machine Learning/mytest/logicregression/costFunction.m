f2750e14dcaf22d12cece8ecdae0c598543c3f63
YUQI <yuqi.sun@outlook.com>
2018-10-25 22:54:36 +0800

    test machine learning

---
+++ Machine Learning/mytest/logicregression/costFunction.m
@@ -0,0 +1,8 @@
+function [J, grad] = costFunction(theta, X, y)
+	m=size(X,1);
+	A1 = (-y).*log(1./(1.+e.^(-X*theta)));
+	A2 = (1.-y).*log(1.-1./(1.+e.^(-X*theta)));
+	J = sum(A1.-A2)/m;
+	h = 1./(1.+e.^(-X*theta));
+	grad = X'*(h.-y)./m;
+end
