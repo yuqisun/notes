71fb4911a38186b2ab7a15cc40089bcd3fabbfa5
YUQI <yuqi.sun@outlook.com>
2019-01-29 21:45:55 +0800

    remove ml

--- Machine Learning/mytest/linearregression/costFunction.m
+++
@@ -1,4 +0,0 @@
-function J = costFunction(theta, X, y)
-	m=length(y);
-	J=(X*theta-y)'*(X*theta-y)/(2*m);
-end
--- Machine Learning/mytest/linearregression/normalEquation.m
+++
@@ -1,7 +0,0 @@
-function theta = normalEquation(X, y, lambda)
-    [m, n]=size(X);
-    L=eye(n, n);
-    L(1, 1)=0;
-	theta=pinv(X'*X+lambda.*L)*X'*y;
-	fprintf('Calculate theta via normal equation with lambda: %f\n', lambda);
-end
